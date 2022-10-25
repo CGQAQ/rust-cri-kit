82e93835915a26865b1e6f21e5eb4646dceadd33
CGQAQ <m.jason.liu@outlook.com>
2022-10-25 08:38:34 +0800

    wow, less magic

--- example/src/lib.rs
+++ example/src/lib.rs
@@ -1,5 +1,1 @@
-use rust_cri_kit::prost;
-use rust_cri_kit::prost::Message;
-use rust_cri_kit::tonic;
-
 mod cri;
--- src/build/transformer.rs
+++ src/build/transformer.rs
@@ -1,83 +1,37 @@
-use quote::__private::Span;
-use quote::{format_ident, quote, ToTokens};
-use syn::visit_mut::{visit_attribute_mut, visit_item_use_mut, visit_path_mut, VisitMut};
-use syn::{parse_file, parse_quote, Meta, Token};
+use syn::visit_mut::{visit_item_trait_mut, visit_trait_item_method_mut, VisitMut};
+use syn::{parse_file, parse_quote};
 
-struct PathTransformer;
+struct TraitItemDefauter;
+impl VisitMut for TraitItemDefauter {
+    fn visit_trait_item_method_mut(&mut self, i: &mut syn::TraitItemMethod) {
+        if i.default.is_none() {
+            let ident = &i.sig.ident;
+            let args = i.sig.inputs.iter().filter_map(|it| match it {
+                syn::FnArg::Receiver(_) => None,
+                syn::FnArg::Typed(pat) => Some(pat.pat.clone()),
+            });
 
-impl VisitMut for PathTransformer {
-    fn visit_path_mut(&mut self, i: &mut syn::Path) {
-        visit_path_mut(self, i);
-
-        if i.leading_colon.is_some() {
-            i.leading_colon = None;
-        }
-        if i.segments.len() >= 1 {
-            let seg = i.segments.first_mut().unwrap();
-            if seg.ident == "prost" || seg.ident == "tonic" {
-                i.segments.insert(0, format_ident!("rust_cri_kit").into());
+            i.default = if i.sig.asyncness.is_some() {
+                Some(parse_quote! {
+                    {self.#ident(#(#args),*).await}
+                })
+            } else {
+                Some(parse_quote! {
+                    {self.#ident(#(#args),*)}
+                })
             }
         }
+        visit_trait_item_method_mut(self, i);
     }
+}
 
-    fn visit_item_use_mut(&mut self, i: &mut syn::ItemUse) {
-        visit_item_use_mut(self, i);
+struct TraitTransformer;
+impl VisitMut for TraitTransformer {
+    fn visit_item_trait_mut(&mut self, i: &mut syn::ItemTrait) {
+        visit_item_trait_mut(self, i);
 
-        if i.leading_colon.is_some() {
-            i.leading_colon = None;
-        }
-
-        i.tree = match i.tree {
-            syn::UseTree::Path(ref mut p) => {
-                if p.ident == "prost" || p.ident == "tonic" {
-                    syn::UseTree::Path(syn::UsePath {
-                        ident: format_ident!("rust_cri_kit"),
-                        tree: Box::new(syn::UseTree::Path(p.clone())),
-                        colon2_token: Token![::](Span::mixed_site()),
-                    })
-                } else {
-                    syn::UseTree::Path(p.clone())
-                }
-            }
-            _ => i.tree.clone(),
-        };
-    }
-
-    fn visit_attribute_mut(&mut self, i: &mut syn::Attribute) {
-        visit_attribute_mut(self, i);
-
-        if i.path.is_ident("derive") {
-            let mut meta = i.parse_meta().unwrap();
-
-            match meta {
-                Meta::List(ref mut ml) => {
-                    if ml.path.is_ident("derive") {
-                        ml.nested.iter_mut().for_each(|it| match it {
-                            syn::NestedMeta::Meta(ref mut m) => match m {
-                                Meta::Path(ref mut p) => {
-                                    if p.leading_colon.is_some() {
-                                        p.leading_colon = None;
-                                    }
-
-                                    if p.segments.first().unwrap().ident == "prost" {
-                                        p.segments.insert(0, format_ident!("rust_cri_kit").into());
-                                    }
-                                }
-                                _ => {}
-                            },
-                            _ => {}
-                        })
-                    }
-
-                    let nested = ml.nested.clone();
-                    i.tokens = proc_macro2::Group::new(
-                        proc_macro2::Delimiter::Parenthesis,
-                        nested.to_token_stream(),
-                    )
-                    .to_token_stream();
-                }
-                _ => {}
-            }
+        if i.ident == "RuntimeService" || i.ident == "ImageService" {
+            TraitItemDefauter.visit_item_trait_mut(i);
         }
     }
 }
@@ -86,7 +40,7 @@ pub fn transform(file_path: String) -> Result<String, Box<dyn std::error::Error>
     let file_content = std::fs::read_to_string(file_path)?;
 
     let mut file_parsed = parse_file(&file_content)?;
-    PathTransformer.visit_file_mut(&mut file_parsed);
+    TraitTransformer.visit_file_mut(&mut file_parsed);
 
     Ok(prettyplease::unparse(&file_parsed))
 }
